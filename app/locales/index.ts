be02aa9100f24dad9599e925987625b6270383e1
Yidadaa <yidadaa@qq.com>
2023-06-15 23:20:14 +0800

    feat: enable drag area for tauri apps

--- app/locales/index.ts
+++ app/locales/index.ts
@@ -66,7 +66,6 @@ function getLanguage() {
   try {
     return navigator.language.toLowerCase();
   } catch {
-    console.log("[Lang] failed to detect user lang.");
     return DEFAULT_LANG;
   }
 }
